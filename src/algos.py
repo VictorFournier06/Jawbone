14cec659105f3382f29c5b6792918c35c42d31c9
Victor Fournier <victor.fournier.06@orange.fr>
2023-12-02 17:48:49 +0100

    transitive closure

--- src/algos.py
+++ src/algos.py
@@ -1,6 +1,6 @@
 #TSP approximation 1
 #find a local minimum order than minimizes the sum of the distances between consecutive sentences (order 1) in O(N^2)
-def greedy_add(distances, permutation = []):
+def insertion_sort(distances, permutation = []):
     permutation = list(permutation)
     #if no permutation is given, we create the default order
     if len(permutation) == 0:
@@ -20,10 +20,10 @@ def greedy_add(distances, permutation = []):
     return(ordered)
 
 #TSP approximation 2
-#alternative algorithm that tries to find a sentence to place better from in O(n^2)
+#alternative algorithm that tries to find a sentence to place better from in O(n^2) - bad algorithm
 def greedy_sort(distances, permutation = []):
     permutation = list(permutation)
-    #if no permutation is given, we start create the default order
+    #if no permutation is given, we create the default order
     if len(permutation) == 0:
         permutation = list(range(len(distances)))
     #for each sentence in the current permutation we try to find a better position
@@ -43,4 +43,78 @@ def greedy_sort(distances, permutation = []):
             permutation.insert(i_min, current)
     return(permutation)
 
-#TSP approximation 3 (good one)
+#TSP approximation 3 (sort - transitivity hypothesis)
+#pairwise_order is a matrix of pairwise orders (0 if i<=j, 1 if i>j)
+def bubble_sort(pairwise_order, permutation = []):
+    permutation = list(permutation)
+    #if no permutation is given, we create the default order
+    if len(permutation) == 0:
+        permutation = list(range(len(pairwise_order)))
+    for i in range(len(permutation)):
+        for j in range(len(permutation)-1):
+            if pairwise_order[permutation[j]][permutation[j+1]]:
+                permutation[j], permutation[j+1] = permutation[j+1], permutation[j]
+    return(permutation)
+
+#TSP approximation 4 (sort - transitivity hypothesis)
+def merge_sort(pairwise_order, permutation = []):
+    permutation = list(permutation)
+    #if no permutation is given, we create the default order
+    if len(permutation) == 0:
+        permutation = list(range(len(pairwise_order)))
+    if len(permutation) > 1:
+        mid = len(permutation)//2
+        left = permutation[:mid]
+        right = permutation[mid:]
+        merge_sort(pairwise_order, left)
+        merge_sort(pairwise_order, right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if pairwise_order[left[i]][right[j]]:
+                permutation[k] = left[i]
+                i += 1
+            else:
+                permutation[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            permutation[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            permutation[k] = right[j]
+            j += 1
+            k += 1
+    return(permutation)
+
+#TSP approximation 4 (sort - transitivity hypothesis)
+def quick_sort(pairwise_order, permutation = []):
+    permutation = list(permutation)
+    #if no permutation is given, we create the default order
+    if len(permutation) == 0:
+        permutation = list(range(len(pairwise_order)))
+    quick_sort_helper(pairwise_order, permutation, 0, len(permutation)-1)
+    return(permutation)
+
+def quick_sort_helper(pairwise_order, permutation, first, last):
+    if first < last:
+        splitpoint = partition(pairwise_order, permutation, first, last)
+        quick_sort_helper(pairwise_order, permutation, first, splitpoint-1)
+        quick_sort_helper(pairwise_order, permutation, splitpoint+1, last)
+        
+def partition(pairwise_order, permutation, first, last):
+    pivotvalue = permutation[first]
+    leftmark = first+1
+    rightmark = last
+    done = False
+    while not done:
+        while leftmark <= rightmark and pairwise_order[permutation[leftmark]][pivotvalue]:
+            leftmark += 1
+        while pairwise_order[pivotvalue][permutation[rightmark]] and rightmark >= leftmark:
+            rightmark -= 1
+        if rightmark < leftmark:
+            done = True
+        else:
+            permutation[leftmark], permutation[rightmark] = permutation[rightmark], permutation[leftmark]
+    permutation[first], permutation[rightmark] = permutation[rightmark], permutation[first]
+    return(rightmark)
--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,7 @@
 #imports
+import numpy as np
 import torch
+import networkx as nx
 
 #function to split the sentences into discard, training, validation and testing sets while keeping the order
 def split_sentences(sentence_embedding, fraction_to_keep = 0.1, fraction_to_train = 0.8, fraction_to_test = 0.5):
@@ -32,7 +34,8 @@ def create_database(sentence_embeddings):
     return torch.cat(X), torch.cat(y)
 
 def create_pairs(sentence_embeddings):
-    sentence_embeddings = torch.tensor(sentence_embeddings).to("cuda")
+    if not(torch.is_tensor(sentence_embeddings) or sentence_embeddings.is_cuda):
+        sentence_embeddings = torch.tensor(sentence_embeddings).to("cuda")
     #create the database
     X = []
     y = []
@@ -47,3 +50,57 @@ def create_pairs(sentence_embeddings):
                 y.append(torch.tensor(j < k, dtype=torch.float).to("cuda"))
     
     return torch.stack(X), torch.stack(y).unsqueeze(1)
+
+#add any diagonal values to the flattened matrix y_pred2
+#convert y_pred2 from a list to a symmetric matrix by averaging the values of the upper and lower triangular
+def pred_to_pairwise(pred):
+    n = np.ceil(pred.shape[0]**0.5).astype(int)
+    L = pred.cpu().detach().numpy().flatten().tolist()
+    if len(L < n*n) :
+        for i in range(n):
+            L.insert(i*i, 0.5)
+    pairwise_probabilities = [[(L[i*n+j]+(1-L[j*n+i]))/2 for i in range(n)] for j in range(n)]
+    return(pairwise_probabilities)
+
+#function taking predictions of pairwise orderings probabilities and returning the order while enforcing transitivity
+#Weighted transitivity closure, weights are the inverse logit of the probabilities
+def weighted_transitivity_closure(pairwise_probs):
+    n = len(pairwise_probs)
+    #create the graph
+    G = nx.DiGraph()
+    G.add_nodes_from(range(n))
+    for i in range(n):
+        for j in range(i+1, n):
+            val = abs(np.log(pairwise_probs[i][j]/(1-pairwise_probs[i][j]))) 
+            weights = 1/val if val >= 1e-7 else float('inf')
+            if pairwise_probs[i][j] > 0.5:
+                G.add_edge(i, j, weight=weights)
+            else:
+                G.add_edge(j, i, weight=weights)
+    #compute the min weight transitive closure with Floyd-Warshall algorithm
+    min_closure = min_weight_transitive_closure(G, weight='weight')
+    
+    #use topological sorting to get the minimal order
+    min_order = list(nx.topological_sort(min_closure))
+    return min_order
+
+def min_weight_transitive_closure(graph, weight='weight'):
+    # Compute the minimum distances between each pair of node using Floyd-Warshall (the smaller the distance the more likely the ordering)
+    min_distances = nx.floyd_warshall(graph, weight=weight)
+
+    # Create a new Directed Graph to represent the minimal weight transitive closure
+    min_closure = nx.DiGraph()
+
+    # Copy nodes from the original graph to min_closure
+    min_closure.add_nodes_from(graph.nodes)
+
+    # Add edges based on the Floyd-Warshall results, ensuring acyclicity
+    for u in graph.nodes:
+        for v in graph.nodes:
+            if u != v and min_distances[u][v] != float('inf') and min_distances[u][v] <= min_distances[v][u]:
+                # Add an edge only if it doesn't create a cycle
+                if not nx.has_path(min_closure, v, u):
+                    min_closure.add_edge(u, v, weight=min_distances[u][v])
+
+    return min_closure
+    
